1ab456bec5d573aea5cc2d8d0b6362091a2c47c1
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-30 20:38:39 +0300

    ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,31 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import js from '@eslint/js'
+import {
+  configureVueProject,
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
+import js from '@eslint/js'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
+configureVueProject({
+  tsSyntaxInTemplates: true,
+  scriptLangs: ['ts', 'js', 'tsx', 'jsx'],
+  allowComponentTypeUnsafety: true,
+  rootDir: import.meta.dirname,
+})
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-  },
-
+export default defineConfigWithVueTs(
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
-])
+
+  pluginVue.configs['flat/essential'],
+
+  vueTsConfigs.base,
+
+  defineConfig({ languageOptions: { globals: { ...globals.browser } } }),
+
+  defineConfig(globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**'])),
+
+  skipFormatting
+)
