80c62084ad83a31bb3bd5ac4e3f56bdff5980459
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-10-07 12:51:29 +0300

    Test getProgressColorClass function

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,5 +1,12 @@
+import { HUNDRED_PERCENT, LOW_PERCENT, MEDIUM_PERCENT } from '@/constants'
 import { expect, it } from 'vitest'
-import { formatSeconds, formatSecondsWithSign, normalizeSelectValue } from '../src/functions'
+import {
+  formatSeconds,
+  formatSecondsWithSign,
+  getProgressColorClass,
+  normalizeSelectValue,
+} from '../src/functions'
+import { ProgressColorClass } from '../src/types'
 
 it('formats seconds with sign', () => {
   expect(formatSecondsWithSign(0)).toBe('+00:00:00')
@@ -32,6 +39,12 @@ it('normalizes select value', () => {
   expect(normalizeSelectValue('1')).toBe(1)
 })
 
-it.todo('gets progress color class')
+it('gets progress color class', () => {
+  expect(getProgressColorClass(0)).toBe(ProgressColorClass.RED)
+  expect(getProgressColorClass(LOW_PERCENT - 1)).toBe(ProgressColorClass.RED)
+  expect(getProgressColorClass(MEDIUM_PERCENT - 1)).toBe(ProgressColorClass.YELLOW)
+  expect(getProgressColorClass(HUNDRED_PERCENT - 1)).toBe(ProgressColorClass.BLUE)
+  expect(getProgressColorClass(100)).toBe(ProgressColorClass.GREEN)
+})
 
 it.todo('generates id')
