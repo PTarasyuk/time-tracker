9033c2e9d639e334057b86d5cec2420dac5439f8
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-10-08 09:44:08 +0300

    Use "describe"

--- test/activities.test.ts
+++ test/activities.test.ts
@@ -1,24 +1,34 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { calculateActivityCompletionPercentage, updateActivity } from '../src/activities'
 import { HUNDRED_PERCENT, SECONDS_IN_HOUR } from '../src/constants'
 import type { Activity } from '../src/types'
 
-it('updates activity', () => {
-  const activity: Activity = {
+describe('updateActivity', () => {
+  const originalActivity: Activity = {
     id: '1',
     name: 'Training',
     secondsToComplete: SECONDS_IN_HOUR * 1,
   }
+
   const updatedFields: Activity = {
     id: '2',
     name: 'Reading',
     secondsToComplete: SECONDS_IN_HOUR * 2,
   }
 
-  const updatedActivity = updateActivity(activity, updatedFields)
+  it('updates original activity', () => {
+    const activity: Activity = { ...originalActivity }
 
-  expect(activity).toEqual(updatedFields)
-  expect(updatedActivity).toEqual(updatedFields)
+    updateActivity(activity, updatedFields)
+
+    expect(activity).toEqual(updatedFields)
+  })
+
+  it('returns updated activity', () => {
+    const activity: Activity = { ...originalActivity }
+
+    expect(updateActivity(activity, updatedFields)).toEqual(updatedFields)
+  })
 })
 
 it('calculate activity completion percentage', () => {
