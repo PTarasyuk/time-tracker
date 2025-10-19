9c3a014e35263222469c4edbf2faf906bdc07c89
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-10-19 16:15:54 +0300

    mount vs shallowMount

--- test/components/BaseButton.spec.ts
+++ test/components/BaseButton.spec.ts
@@ -1,6 +1,4 @@
-// @vitest-environment happy-dom
-
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { expect, it } from 'vitest'
 import BaseButton from '../../src/components/BaseButton.vue'
 import { ButtonType } from '../../src/types'
@@ -8,7 +6,7 @@ import { ButtonType } from '../../src/types'
 it("renders 'primary' button by default", () => {
   const primaryButtonClasses = 'bg-purple-500 enabled:hover:bg-purple-600 text-white'
 
-  const wrapper = mount(BaseButton)
+  const wrapper = shallowMount(BaseButton)
 
   expect(wrapper.classes()).toEqual(expect.arrayContaining(primaryButtonClasses.split(' ')))
 })
@@ -37,13 +35,13 @@ it.each([
 ])('renders $type button with label', ({ type, classes }) => {
   const label = 'Label'
 
-  const wrapper = mount(BaseButton, {
+  const wrapper = shallowMount(BaseButton, {
     props: { type },
     slots: {
       default: label,
     },
   })
 
-  expect(wrapper.html()).toContain(label)
+  expect(wrapper.text()).toContain(label)
   expect(wrapper.classes()).toEqual(expect.arrayContaining(classes.split(' ')))
 })
