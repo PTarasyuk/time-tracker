e35a7381bbbfa23c598f7b5ae5ef2ffc24d02e18
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-10-19 16:36:35 +0300

    BaseIcon component test

---
+++ test/components/BaseIcon.spec.ts
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils'
+import { expect, it } from 'vitest'
+import BaseIcon from '../../src/components/BaseIcon.vue'
+import { IconName } from '../../src/types'
+
+it('renders icon default classes', () => {
+  const wrapper = shallowMount(BaseIcon, {
+    props: { name: IconName.CLOCK },
+  })
+
+  expect(wrapper.classes()).toContain('h-8')
+})
+
+it('renders icon default classes', () => {
+  const classes = 'h-12 text-purple-600'
+  const wrapper = shallowMount(BaseIcon, {
+    attrs: { class: classes },
+    props: { name: IconName.CLOCK },
+  })
+
+  expect(wrapper.classes().join(' ')).toBe(classes)
+})
